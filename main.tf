ec2952fb45f87fd1cbfabecea7fea72be2e26479
InfraAsCodeDemo <64269201+InfraAsCodeDemo@users.noreply.github.com>
2020-04-27 17:18:24 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,30 @@
+//--------------------------------------------------------------------
+// Variables
+variable "simplevm_clientId" {}
+variable "simplevm_clientSecret" {}
+variable "simplevm_hradwareType" {}
+variable "simplevm_network" {}
+variable "simplevm_region" {}
+variable "simplevm_rgName" {}
+variable "simplevm_subnet" {}
+variable "simplevm_subscriptionId" {}
+variable "simplevm_tenantId" {}
+variable "simplevm_vmname" {}
+
+//--------------------------------------------------------------------
+// Modules
+module "simplevm" {
+  source  = "app.terraform.io/custdemo/simplevm/azure"
+  version = "1.0.1"
+
+  clientId = "${var.simplevm_clientId}"
+  clientSecret = "${var.simplevm_clientSecret}"
+  hradwareType = "${var.simplevm_hradwareType}"
+  network = "${var.simplevm_network}"
+  region = "${var.simplevm_region}"
+  rgName = "${var.simplevm_rgName}"
+  subnet = "${var.simplevm_subnet}"
+  subscriptionId = "${var.simplevm_subscriptionId}"
+  tenantId = "${var.simplevm_tenantId}"
+  vmname = "${var.simplevm_vmname}"
+}
